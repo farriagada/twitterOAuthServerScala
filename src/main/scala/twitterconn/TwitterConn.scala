a299123c6b7da99b10cdfbcfaf361c983f26817b
Francisco <pinwy.bartok@gmail.com>
2018-08-30 22:28:21 -0300

    Twitter Sign-In implementation completed 100%.

--- src/main/scala/twitterconn/TwitterConn.scala
+++ src/main/scala/twitterconn/TwitterConn.scala
@@ -1,34 +1,48 @@
 package twitterconn
 
-import java.io.UnsupportedEncodingException
+
 import java.net.URLEncoder
 import java.util.{Base64, Date}
 
 import akka.actor.ActorSystem
 import akka.http.scaladsl.Http
 import akka.http.scaladsl.model._
-import akka.http.scaladsl.model.headers.{BasicHttpCredentials, RawHeader}
 import akka.stream.ActorMaterializer
 import akka.http.scaladsl.server.Directives._
 import ch.megard.akka.http.cors.scaladsl.CorsDirectives._
 import ch.megard.akka.http.cors.scaladsl.settings.CorsSettings
 import com.roundeights.hasher.Implicits._
 import java.io.UnsupportedEncodingException
-import scala.util.parsing.json._
-import scala.concurrent.{Await, Future}
-import scala.concurrent.duration.Duration
+import java.util.concurrent.TimeUnit
+
+import scala.concurrent.{Await, Future, TimeoutException}
 import javax.crypto.Mac
 import javax.crypto.spec.SecretKeySpec
-import akka.util.ByteString
 
+import akka.Done
+import akka.http.scaladsl.model.StatusCodes.Redirection
+import akka.http.scaladsl.model.headers.{Cookie, HttpCookie, HttpCookiePair, HttpOrigin}
+import akka.http.scaladsl.server.RejectionError
+import akka.http.scaladsl.unmarshalling.Unmarshal
+import akka.stream.scaladsl.Source
+import akka.util.ByteString
+import akka.util.Timeout
+
+import collection.immutable.Seq
 import scala.language.postfixOps
 import scala.util.{Failure, Random, Success}
 
 object TwitterConn {
-  private val consumerKey:String = "9ZbFMEJoI8smW7wxmYy1AFjBn"
+  private val consumerKey:String = "AE9dKFE0qkBX2AWLskj8kPlQg"
   private var oauth_nonce:String = null
+  private var new_nonce:String = null
+  private var third_nonce:String = null
   private var oauth_timestamp: String = null
-  private val consumerSecret: String = "29qvrnCep9HLrww19rTHjOcEWGlbpaOCsnhHcj5OZGHL9H8Se9"
+  private var new_timestamo: String = null
+  private val consumerSecret: String = "KqoUebfGv0cEsazgjUfdmu4MlwIv60m4dQ1nLDI6gxcblUUh1F"
+  private var oauth_verifier = "null"
+  private var oauth_token = "null"
+  private var oauth_token_secret = "null"
 
   def encodeUriComp(s: String): String = {
     var result:String = null
@@ -59,8 +73,7 @@ object TwitterConn {
     return (System.currentTimeMillis()/1000).toString()
   }
 
-  def createSignature(): String = {
-    val callback = this.encodeUriComp("http://127.0.0.1:4200/profile/083a3d5f-3966-4435-90f6-99461c467252")
+  def createSignatureRequestToken(callback: String): String = {
     val rawURL: String = "POST&" + this.encodeUriComp("https://api.twitter.com/oauth/request_token")+"&"
     val parameterString: String = "oauth_callback=" + callback +
                                   "&oauth_consumer_key=" + this.consumerKey +
@@ -71,14 +84,98 @@ object TwitterConn {
 
     val signingString = rawURL + this.encodeUriComp(parameterString)
     val signingKey = this.encodeUriComp(this.consumerSecret) + "&" // No token secret because its Request_Token
-    println("Hmac: " + (signingString.hmac(signingKey)).sha1.hex)
     val secret = new SecretKeySpec(signingKey.getBytes("UTF-8"), "HmacSHA1") // We create the key
     val mac = Mac.getInstance("HmacSHA1")
     mac.init(secret) // We init the HmacSHA1
     val hmac = mac.doFinal(signingString.getBytes("UTF-8")) // We create the Hmac with the string
     val signatur = Base64.getEncoder.encodeToString(hmac) // We Base64 encode the Hmac.
-    println("Signatur: " +signatur)
     return signatur
+  }
+
+  def createSignatureAccessToken(): String = {
+    val rawURL: String = "POST&" + this.encodeUriComp("https://api.twitter.com/oauth/access_token")+"&"
+    val parameterString: String =
+      "oauth_consumer_key=" + this.consumerKey +
+      "&oauth_nonce=" + this.new_nonce +
+      "&oauth_signature_method=HMAC-SHA1"+
+      "&oauth_timestamp=" + this.oauth_timestamp +
+      "&oauth_token=" + this.oauth_token +
+      "&oauth_version=1.0"
+
+    val signingString = rawURL + this.encodeUriComp(parameterString)
+    val signingKey = this.encodeUriComp(this.consumerSecret) + "&" + this.encodeUriComp(this.oauth_token_secret)
+    val secret = new SecretKeySpec(signingKey.getBytes("UTF-8"), "HmacSHA1") // We create the key
+    val mac = Mac.getInstance("HmacSHA1")
+    mac.init(secret) // We init the HmacSHA1
+    val hmac = mac.doFinal(signingString.getBytes("UTF-8")) // We create the Hmac with the string
+    val signatur = Base64.getEncoder.encodeToString(hmac) // We Base64 encode the Hmac.
+    return signatur
+  }
+
+  def createSignatureUser(): String = {
+    val rawURL: String = "GET&" + this.encodeUriComp("https://api.twitter.com/1.1/account/verify_credentials.json")+"&"
+    val parameterString: String =
+      "oauth_consumer_key=" + this.consumerKey +
+        "&oauth_nonce=" + this.third_nonce +
+        "&oauth_signature_method=HMAC-SHA1"+
+        "&oauth_timestamp=" + this.new_timestamo +
+        "&oauth_token=" + this.oauth_token +
+        "&oauth_version=1.0"
+    val signingString = rawURL + this.encodeUriComp(parameterString)
+    val signingKey = this.encodeUriComp(this.consumerSecret) + "&" + this.encodeUriComp(this.oauth_token_secret)
+    val secret = new SecretKeySpec(signingKey.getBytes("UTF-8"), "HmacSHA1") // We create the key
+    val mac = Mac.getInstance("HmacSHA1")
+    mac.init(secret) // We init the HmacSHA1
+    val hmac = mac.doFinal(signingString.getBytes("UTF-8")) // We create the Hmac with the string
+    val signatur = Base64.getEncoder.encodeToString(hmac) // We Base64 encode the Hmac.
+    return signatur
+  }
+
+  def setRequestTokenRSP(str: String): Array[String] = {
+    var i, j = 0
+    var token, secret, callback = ""
+    str.split("&").foreach(sub =>{
+      sub.split("=").foreach(subsub => {
+          if(i==0){ //oauth_token
+            if(j==1) token = subsub
+          }
+          if(i==1){ //oauth_token_secret
+            if(j==1) secret = subsub
+          }
+          if(i==2){ //oauth_callback_confirmed
+            if(j==1) callback = subsub
+          }
+        j=j+1
+      })
+      j=0
+      i=i+1
+    })
+    return Array(token,secret,callback)
+  }
+
+  def makeAccessJSON(str: String): Array[String] = {
+    var i, j = 0
+    var token, secret,user_id, screen_name = ""
+    str.split("&").foreach(sub =>{
+      sub.split("=").foreach(subsub => {
+        if(i==0){ //oauth_token
+          if(j==1) token = subsub
+        }
+        if(i==1){ //oauth_token_secret
+          if(j==1) secret = subsub
+        }
+        if(i==2){ //oauth_callback_confirmed
+          if(j==1) user_id = subsub
+        }
+        if(i==3){
+          if(j==1) screen_name = subsub
+        }
+        j=j+1
+      })
+      j=0
+      i=i+1
+    })
+    return Array(token,secret,user_id,screen_name)
   }
 
   def main(args: Array[String]): Unit ={
@@ -90,43 +187,132 @@ object TwitterConn {
 
     val settings = CorsSettings.defaultSettings.withAllowCredentials(false)
     val route = cors(settings){
-      path("StepOne"){
-        parameters('callback.as[String])(cb => {
-          val callback = this.encodeUriComp(cb)
-          val url = "https://api.twitter.com/oauth/request_token"
-          var response: String = null
-          this.oauth_timestamp = this.createTimestamp()
-          this.oauth_nonce = this.randomString(32)
-          val authorization = headers.RawHeader("Authorization",
-            """OAuth oauth_callback="""" + callback +
-              """", oauth_consumer_key="""" + this.consumerKey +
-              """", oauth_nonce="""" + this.oauth_nonce +
-              """", oauth_signature="""" + this.encodeUriComp(this.createSignature()) +
-              """", oauth_signature_method="HMAC-SHA1", oauth_timestamp="""" + this.oauth_timestamp +
-              """", oauth_version="1.0"""")
-          val params = ByteString(callback)
-          var jsonRSP = "null"
-          val responseFuture: Future[HttpResponse] = Http().singleRequest(HttpRequest(HttpMethods.POST, url,
-            headers = List(authorization),
-            entity = HttpEntity(ContentTypes.`text/plain(UTF-8)`, params)))
-          responseFuture
-            .onComplete {
-              case Success(res) => {
-                val response = res._3.dataBytes.map(_.utf8String).runForeach(body => {
-                  /* We postedit the string to make it JSON Parsable */
-                  val postBody = "{"+body.replaceAll("=",":").replaceAll("&",",").replaceAll("(\\w+)", "\"$1\"")+"}"
-                  jsonRSP = postBody
-                  println(postBody)
-                })
+
+      path("requestToken"){
+        get {
+          parameters('callback.as[String])(cb => {
+              val callback = this.encodeUriComp(cb)
+              val url = "https://api.twitter.com/oauth/request_token"
+              this.oauth_timestamp = this.createTimestamp()
+              this.oauth_nonce = this.randomString(32)
+              val authorization = headers.RawHeader("Authorization",
+                """OAuth oauth_callback="""" + callback +
+                  """", oauth_consumer_key="""" + this.consumerKey +
+                  """", oauth_nonce="""" + this.oauth_nonce +
+                  """", oauth_signature="""" + this.encodeUriComp(this.createSignatureRequestToken(callback)) +
+                  """", oauth_signature_method="HMAC-SHA1", oauth_timestamp="""" + this.oauth_timestamp +
+                  """", oauth_version="1.0"""")
+              val params = ByteString(callback)
+              val responseFuture: Future[HttpResponse] = Http().singleRequest(HttpRequest(HttpMethods.POST, url,
+                  headers = List(authorization),
+                  entity = HttpEntity(ContentTypes.`text/plain(UTF-8)`, params)))
+              onComplete(responseFuture) {
+                  case Success(result) => {
+                      // We cut off "HttpEntity.Strict(text/html; charset=UTF-8" from the result string. If anything changes in Akka, this must be changed too.
+                      val Str = result.entity.httpEntity.toString()
+                      val response = Str.substring(Str.indexOf("o"), Str.indexOf(")")) //This is the final response.
+                      val arr = setRequestTokenRSP(response)
+                      this.oauth_token = arr(0)
+                      this.oauth_token_secret = arr(1)
+                      if (arr(2) == "true") { // if oauth_callback_confirmed == true
+                          val uri = "https://api.twitter.com/oauth/authenticate?oauth_token=" + arr(0)
+                          complete(HttpEntity(ContentTypes.`text/plain(UTF-8)`, uri))
+                      }
+                      else complete("Error receiving Request Token")
+                  }
+                  case Failure(ex) => complete(HttpEntity(ContentTypes.`text/plain(UTF-8)`, "Couldn't process request. Exception is: " +ex))
               }
-              case Failure(_) => sys.error("Couldn't get into api.twitter")
+          })
+        }
+      }~
+      path("TwitterRedirect"){
+        get{
+          parameters('oauth_verifier.as[String], 'oauth_token.as[String])((verifier, token) => {
+            if(this.oauth_token==token) { // Check whether token received in authorization equals the previous one.
+              this.oauth_verifier = verifier
+              val rspToAngular = verifier + "|" + token
+              val html =
+                "<!doctype html>" +
+                  "<html lang='en'>" +
+                  "<head>" +
+                  "<meta charset='UTF-8'> <title>Popup</title> </head>" +
+                  "<body> <script type='text/javascript'>" +
+                  "window.opener.postMessage('" + rspToAngular + "', 'http://localhost:4200');" +
+                  "</script></body></html>"
+              complete(HttpEntity(ContentTypes.`text/html(UTF-8)`, html))
             }
-          Await.result(responseFuture, Duration.Inf)
-          //s"response: " + response + s"!"
-          complete(HttpResponse(StatusCodes.OK, entity = HttpEntity(ContentTypes.`application/json`,  jsonRSP)))
-        })
+            else {
+              val html =
+                "<!doctype html>" +
+                  "<html lang='en'>" +
+                  "<head>" +
+                  "<meta charset='UTF-8'> <title>Popup</title> </head>" +
+                  "<body> <script type='text/javascript'>" +
+                  "window.opener.postMessage('Error: Token Mismatch', 'http://localhost:4200');" +
+                  "</script></body></html>"
+              complete(HttpEntity(ContentTypes.`text/html(UTF-8)`,html))
+            }
+          })
+        }
+      }~
+      path("getAccessToken"){
+        get{
+          parameters('verifier.as[String], 'token.as[String])((verifier,token) => {
+            val url = "https://api.twitter.com/oauth/access_token"
+            this.new_nonce = this.randomString(32)
+            val authorization = headers.RawHeader("Authorization",
+                """OAuth oauth_consumer_key="""" + this.consumerKey +
+                """", oauth_nonce="""" + this.new_nonce +
+                """", oauth_signature="""" + this.encodeUriComp(this.createSignatureAccessToken()) +
+                """", oauth_signature_method="HMAC-SHA1", oauth_timestamp="""" + this.oauth_timestamp +
+                """", oauth_token=""""+ token +
+                """", oauth_version="1.0"""")
+            val responseFuture: Future[HttpResponse] = Http().singleRequest(HttpRequest(HttpMethods.POST, url,
+              headers = List(authorization),
+              entity = FormData(Map("oauth_verifier" -> verifier)).toEntity(HttpCharsets.`UTF-8`)))
+            onComplete(responseFuture){
+              case Success(res) => {
+                val Str = res.entity.httpEntity.toString()
+                val response = Str.substring(Str.indexOf("o"), Str.indexOf(")")) //This is the final response.
+                val arr = makeAccessJSON(response)
+                val jsonRSP = """ {"access_token":""""+arr(0)+"""" , "token_secret":""""+arr(1)+"""" ,"user_id":""""+arr(2)+"""" , "screen_name":""""+arr(3)+""""}"""
+                complete(HttpEntity(ContentTypes.`text/plain(UTF-8)`,jsonRSP))
+              }
+              case Failure(ex) => complete(s"Error Occurred")
+            }
+          })
+        }
+      }~
+      path("verifyCredentials"){
+        get{
+          parameters('token.as[String],'tokenSecret.as[String])((token, tokenSecret) => {
+            this.oauth_token = token
+            this.oauth_token_secret = tokenSecret
+            this.third_nonce = this.randomString(32)
+            this.new_timestamo = this.createTimestamp()
+            val url = "https://api.twitter.com/1.1/account/verify_credentials.json"
+            val authorization = headers.RawHeader("Authorization",
+              """OAuth oauth_consumer_key="""" + this.consumerKey +
+                """", oauth_nonce="""" + this.third_nonce +
+                """", oauth_signature="""" + this.encodeUriComp(this.createSignatureUser()) +
+                """", oauth_signature_method="HMAC-SHA1", oauth_timestamp="""" + this.new_timestamo +
+                """", oauth_token=""""+ token +
+                """", oauth_version="1.0"""")
+            val request: Future[HttpResponse] = Http().singleRequest(HttpRequest(HttpMethods.GET, url, headers = List(authorization)))
+            onComplete(request) {
+              case Success(res) => {
+                val Str = res.entity.httpEntity.toString
+                val response = Str.substring(Str.indexOf("{"), (Str.length()-1)) //This is the final response.
+                complete(HttpEntity(ContentTypes.`application/json`, response))
+              }
+              case Failure(ex) => complete("Couldn't get user info. Reason:  " + ex)
+            }
+          })
+        }
       }
+      /** END OF ROUTING **/
     }
-    Http().bindAndHandle(route, "localhost", 8081)
+    Http().bindAndHandle(route, "127.0.0.1", 8081)
+
   }
 }
